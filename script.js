ae8a8abf97fddd807aa475efd033003943d7bfa7
星辰iyun <148077545+511655084@users.noreply.github.com>
2025-03-16 16:04:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// 页面加载时设置延迟弹窗
-window.addEventListener('load', function () {
-    setTimeout(() => {
-        alert('你好，我是星辰iyun。');
-    }, 3000); // 延迟 3 秒显示弹窗
-});
-
 // 获取第二个模块的链接
 const secondModuleLink = document.getElementById('second-module-link');
 
