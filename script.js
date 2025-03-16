eee5320a6a7d22bf27e34bc5febc1fbffb70b168
星辰iyun <148077545+511655084@users.noreply.github.com>
2025-03-16 15:36:06 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// 侧边栏切换功能
-const sidebarToggle = document.querySelector('.sidebar-toggle');
-const sidebar = document.querySelector('.sidebar');
-
-sidebarToggle.addEventListener('click', function () {
-    sidebar.classList.toggle('active');
-});
-
 // 页面加载时弹出欢迎信息
 window.addEventListener('load', function () {
     alert('你好，我是星辰iyun。');
