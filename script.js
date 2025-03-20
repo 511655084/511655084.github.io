d723f5847d51560a803a79983256180200b88d73
星辰iyun <148077545+511655084@users.noreply.github.com>
2025-03-20 15:42:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     // 获取所有模块的链接元素
     const modules = document.querySelectorAll('.module a');
 
     // 为每个模块链接添加点击事件监听器
-    for (let i = 0; i < modules.length; i++) {
-        modules[i].addEventListener('click', function (e) {
+    modules.forEach(module => {
+        module.addEventListener('click', function (e) {
             e.preventDefault();
             window.open(this.href, '_blank');
         });
-    }
+    });
 });
