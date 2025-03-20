139d47808638d4516489c09af525823a5eeaa058
星辰iyun <148077545+511655084@users.noreply.github.com>
2025-03-20 12:56:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-// 获取第二个模块的链接
-const secondModuleLink = document.getElementById('second-module-link');
+// 获取所有模块的链接元素
+const modules = document.querySelectorAll('.module a');
 
-// 为第二个模块链接添加点击事件监听器
-secondModuleLink.addEventListener('click', function (e) {
-    e.preventDefault();
-    // 直接打开网站
-    window.open('http://www.98qy.com/sjbz/api.php', '_blank');
-});    
+// 为每个模块链接添加点击事件监听器
+modules.forEach(module => {
+    module.addEventListener('click', function (e) {
+        e.preventDefault();
+        window.open(this.href, '_blank');
+    });
+});
